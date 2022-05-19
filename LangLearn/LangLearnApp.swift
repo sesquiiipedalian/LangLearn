ccdfb4f58ba31c46f77f013db10471aedf41beaa
kemal azamat <akemal@edu.hse.ru>
2022-05-19 21:57:27 +0300

    Initial Commit

---
+++ LangLearn/LangLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangLearnApp.swift
+//  LangLearn
+//
+//  Created by Азамат Кемал on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct LangLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
